cb7412e3184ec0bc538655552433e81ff0c2ea1d
JosCanPech <132014654+JosCanPech@users.noreply.github.com>
2024-05-05 18:42:34 -0600

    Update NodoBin.java

--- Arbol/NodoBin.java
+++ Arbol/NodoBin.java
@@ -1,3 +1,25 @@
 public class NodoBin {
     
+    NodoBin Anterior;
+    NodoBin Siguiente;
+    
+    NodoBin Link;
+    
+    
+    public NodoBin getAnterior(){
+        return Anterior;
+    }
+    
+    public void setAnterior(NodoBin Anterior){
+        this.Anterior = Anterior;
+    }
+    
+    public NodoBin getSiguiente(){
+        return Siguiente;
+    }
+    
+    public void setSiguiente(NodoBin Siguiente){
+        this.Siguiente = Siguiente;
+    }
+    
 }
