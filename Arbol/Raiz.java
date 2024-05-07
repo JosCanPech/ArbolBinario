2288f8af70da83c1e64a87898d92bc9d504985ee
JosCanPech <132014654+JosCanPech@users.noreply.github.com>
2024-05-06 18:45:59 -0600

    Update Raiz.java

--- Arbol/Raiz.java
+++ Arbol/Raiz.java
@@ -1,3 +1,51 @@
 public class Raiz {
     
-}
+    NodoBin raiz;
+    
+    public Raiz(){
+        raiz = null;
+    }
+    
+    public boolean Vacio(){
+        return raiz == null;
+    }
+    
+    public void Insertar(String Val){
+        NodoBin nuevo = new NodoBin(Val);
+        if(raiz  == null){
+            raiz = nuevo;
+        }else{
+            NodoBin Temp = raiz;
+            NodoBin principal;
+            while(true){
+                principal = Temp;
+                if(Val.compareTo(Temp.Info)<=0){
+                    Temp = Temp.Izquierda;
+                    if(Temp == null){
+                        principal.Izquierda = nuevo;
+                        return;
+                    }
+                    
+                }else{
+                    Temp = Temp.Derecha;
+                    if(Temp == null){
+                        principal.Derecha = nuevo;
+                        return;
+                    }
+                }
+            }
+            
+            
+        }
+    }
+    
+  public void RecInOrden(NodoBin Value){
+      if(Value != null){
+          RecInOrden(Value.Izquierda);
+          System.out.print(Value.Info+", ");
+          RecInOrden(Value.Derecha);
+      }
+     }
+      
+  }
+   
