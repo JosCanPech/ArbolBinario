ae5c0f226e08e1d77d8a3297716b39d4c1399595
JosCanPech <132014654+JosCanPech@users.noreply.github.com>
2024-05-06 18:47:27 -0600

    Update NodoBin.java

--- Arbol/NodoBin.java
+++ Arbol/NodoBin.java
@@ -1,25 +1,83 @@
 public class NodoBin {
     
-    NodoBin Anterior;
-    NodoBin Siguiente;
+    /*Object elemento;
+    
+    NodoBin Izquierdo;
+    NodoBin Derecho;
     
     NodoBin Link;
     
+    int Info;
     
-    public NodoBin getAnterior(){
-        return Anterior;
+    
+    public NodoBin(Object e){
+        this(e,null, null);
+        
     }
     
-    public void setAnterior(NodoBin Anterior){
-        this.Anterior = Anterior;
+    public NodoBin(Object e, NodoBin Anterior, NodoBin Siguiente){
+        elemento = e;
+        Anterior = Anterior;
+        Siguiente = Siguiente;
     }
     
-    public NodoBin getSiguiente(){
-        return Siguiente;
+    
+    
+    
+    public NodoBin getIzquierdo(){
+        return Izquierdo;
     }
     
-    public void setSiguiente(NodoBin Siguiente){
-        this.Siguiente = Siguiente;
+    /*
+    public void setIzquierdo(NodoBin Izquierdo){
+        this.Izquierdo = Izquierdo;
+    }*/
+    /*
+    public NodoBin getDerecho(){
+        return Derecho;
     }
+    
+    /*public void setDerecho(NodoBin Derecho){
+        this.Derecho = Derecho;
+    }*/
+   /* 
+    public int getInfo(){
+        return Info;
+    }
+    
+    public void setInfo(int Info){
+        this.Info = Info;
+    }
+    
+    public NodoBin(int Info){
+        this.Info = Info;
+        this.Izquierdo = null;
+        this.Derecho = null;
+    }
+    
+    public void Algoritmo(int info){
+        if(2){
+            
+        }else{
+            
+        }
+        
+    }*/
+    
+    String Info;
+    NodoBin Izquierda;
+    NodoBin Derecha;
+    
+    
+    public NodoBin(String Info){
+        this.Info = Info;
+        this.Izquierda = null;
+        this.Derecha = null;
+    }
+    
+    public String toString(){
+        return "\n El nombre del nodo es: "+Info;
+    }
+    
     
 }
